953c3c9a1365ec2d30cb159d5205c7f9af4c00fc
Koh Jia Xian <kohjx96@gmail.com>
2022-07-14 22:11:07 +0800

    Add dropdown to test button

--- src/components/DeckManager/SideBar/SideBar.tsx
+++ src/components/DeckManager/SideBar/SideBar.tsx
@@ -1,27 +1,112 @@
-import { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
+import { useState, useRef } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Paper,
+  Grow,
+  ClickAwayListener,
+  MenuList,
+  Popper,
+  MenuItem,
+ } from '@mui/material';
+ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-import styles from './SideBar.module.css';
+// import styles from './SideBar.module.css';
 
-export default function SideBar(
-  {
+export default function SideBar() {
+
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    console.info(`Start test`);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const testSelectedCards = () => {
+    console.info("Test selected cards");
   }
-  : {
+
+  const testAllCards = () => {
+    console.info("Test all cards");
   }
-) {
 
   return (
-    <Button 
-      variant="contained"
-      color="primary"
-      sx={{
-        fontSize: "2rem",
-        height: "20%",
-        width: "100%",
-        borderRadius: "25px",
-      }}
-    >
-      Start Test
-    </Button>
+    <>
+      <ButtonGroup ref={anchorRef} 
+        sx={{ 
+          width: "100%", 
+          height: "20%",
+        }}
+      >
+        <Button 
+          variant="contained"
+          color="primary"
+          sx={{
+            width: "80%",
+            fontSize: "2rem",
+          }}
+          onClick={handleClick}
+        >
+          Start Test
+        </Button>
+        <Button
+            variant="contained"
+            onClick={handleToggle}
+            color="primary"
+            sx={{
+              width: "20%",
+              fontSize: "2rem",
+            }}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+      </ButtonGroup>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        placement="bottom-end"
+        transition
+        disablePortal
+      >
+        {({ TransitionProps }) => (
+          <Grow
+            {...TransitionProps}
+            style={{ transformOrigin: 'center top' }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                    <MenuItem
+                      onClick={testSelectedCards}
+                    >
+                      Select Cards to Test
+                    </MenuItem>
+                    <MenuItem
+                      onClick={testAllCards}
+                    >
+                      Test using all cards
+                    </MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
   );
 }
